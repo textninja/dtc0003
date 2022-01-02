edacffc3b41a458d2463ae55a683c2e84e87fc61
Joe Taylor <joe@textninja.net>
2022-01-01 17:19:16 -0800

    Added moving stripes

--- movingstripes/main.js
+++ movingstripes/main.js
@@ -1,6 +1,13 @@
-import './style.css'
+import './style.scss'
+import { gsap } from 'gsap';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+
+let timeline = gsap.timeline().repeat(-1);
+timeline
+    .to("html", { "--col2": "2fr", duration: 5 })
+    .to("html", { "--col2": "1fr", duration: 5 });
+
+let timeline2 = gsap.timeline().repeat(-1);
+timeline2
+    .to("html", { "--col4": "0.5fr", duration: 3 })
+    .to("html", { "--col4": "1fr", duration: 3 })
